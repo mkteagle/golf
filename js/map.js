6448182a1bb656dbd198687413fc67286d4080c6
Michael Teagle <i68066@mteagle-g3qp.xactware.com>
2015-10-23 13:17:25 -0600

    added ajax

--- js/map.js
+++ js/map.js
@@ -1,12 +1,57 @@
-function myFunction() {
-    var mymap = document.getElementById("map");
-    var numbPins = 50;
-    var pinCollection = '';
-
-    for (var i = 0; i < numbPins; i++) {
-        var randomTop = Math.floor((Math.random() * 400) + 1);
-        var randomLeft = Math.floor((Math.random() * 300) + 1);
-        pinCollection += "<div class='pin'" + "style='top:" + randomTop + "px; left:" + randomLeft + "px;'>" + "<img src='../img/pin.png' height='30px' width='20px'>" + "</div>"
-    }
-    mymap.innerHTML = pinCollection;
+//function myFunction() {
+//
+//}
+var redirectURI = document.URL;
+var clientID = "a88e7b45-fa45-4d05-829e-975681adc580";
+var accessToken = getUrlVars().access_token;
+var swingBySwing = "https://api.swingbyswing.com/v2/oauth/authorize?scope=read&redirect_uri=" + encodeURI(redirectURI) + "&response_type=token&client_id=" + clientID;
+if(accessToken == null) {
+    location.replace(swingBySwing);
 }
+else {
+    accessToken = accessToken.replace("\n","");
+    model = getCourse(51763);
+}
+
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+        function (m, key, value) {
+            vars[key] = value;
+        });
+    return vars;
+}
+
+var map;
+var infowindow;
+
+function initMap() {
+    var oldAndrews = {lat: 56.357552, lng: -2.818553};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: oldAndrews,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+    var marker = new google.maps.Marker({
+        position: oldAndrews,
+        map: map,
+        title: "Old St. Andrews"
+    });
+
+}
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://api.swingbyswing.com/v2/courses/51763?includes=practice_area,nearby_courses,recent_media,recent_comments,recent_rounds,best_rounds,current_rounds,course_stats_month,course_stats_year&access_token=" + accessToken, false);
+    xhttp.send();
+    document.getElementById("courseInfo").innerHTML = xhttp.responseText;
+}
+//    function Map () {
+//        //takes in a hole and displays it
+//        this.showLocation(){
+//
+//        }
+//    }
+
