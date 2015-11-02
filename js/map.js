27279fc31f6657dfb89f4b15360fde7b25bd362e
Michael Teagle <mkteagle@gmail.com>
2015-11-01 22:59:59 -0700

    made some changes to a table and started a test html file.

--- js/map.js
+++ js/map.js
@@ -9,6 +9,9 @@ var map;
 var markers = [];
 var rindex = 4;
 var hindex = 1;
+var playerRow = 4;
+var prRows = 5;
+var alertTimerId = 0;
 var accessToken = getUrlVars().access_token;
 var swingBySwing = "https://api.swingbyswing.com/v2/oauth/authorize?scope=read&redirect_uri=" + encodeURI(redirectURI) + "&response_type=token&client_id=" + clientID;
 if (accessToken == null) {
@@ -54,9 +57,11 @@ function initMap(location) {
         title: "Old St. Andrews"
     });
 }
+
 function submitSettings(holecount, tee) {
     var tee = document.getElementById("tee").value;
     var holecount = document.getElementById("holecount").value;
+    removeTees(tee);
     addHoles(holecount);
     addTeeBox(holecount, tee);
     if (holecount == 18) {
@@ -128,34 +133,47 @@ function setMapOnAll(map) {
 function clearMarkers() {
     setMapOnAll(null);
 }
-
 function deleteMarkers() {
     clearMarkers();
     markers = [];
 }
-
-function addRow(holecount, tee) {
-    editableGrid.append(editableGrid.data.length, { 'Author': 'ME' }, true);
-    for (var i = 0; i < 18; i++) {
-        $('.editablegrid-hole'+ hindex).addClass('hole'+ hindex);
-        if (hindex++ >= 10) {
-            $('.editablegrid-hole'+ hindex).addClass('hide');
-            $('.editablegrid-totals9').addClass('hide');
-        }
-        hindex++;
-    }
-    $('.editablegrid-name').addClass('playersName');
-    $('.editablegrid-totals9').addClass('totals9');
-    $('.editablegrid-totals18').addClass('totals18');
-
-    if (holecount == 18) {
-        addClass(holecount);
-    }
-    if (holecount == 9) {
-        removeClass(holecount, tee);
-    }
-    rindex++;
-}
+//function addRow(holecount, tee) {
+//    var holed = document.getElementById('holecount').value;
+//    editableGrid.append(editableGrid.data.length, { 'Author': 'ME' }, true);
+//    $('.editablegrid-name').addClass('playersName');
+//    $('.editablegrid-totals9').addClass('totals9');
+//    //document.getElementById("#"+playerRow).id="pr"+prRows;
+//    //prRows++;
+//    //playerRow++;
+//
+//    if (holed == 18) {
+//        $('.editablegrid-totals9').addClass('totals9 hide').addId();
+//        $('.editablegrid-totals18').addClass('totals18 show-cell');
+//        for (var i = 1; i <= 9; i++) {
+//            $('.editablegrid-hole'+ hindex).addClass('show-cell hole'+ hindex);
+//            hindex++;
+//        }
+//        for (var i = 9; i < 19; i++) {
+//            $('.editablegrid-hole'+ hindex).addClass('show-cell hole' + hindex);
+//            hindex++;
+//        }
+//    }
+//
+//    if (holed == 9 || holed == undefined || holed == '') {
+//        $('.editablegrid-totals18').addClass('totals18 hide');
+//        $('.editablegrid-totals9').removeClass('hide').addClass('show-cell');
+//        for (var i = 1; i <= 9; i++) {
+//            $('.editablegrid-hole'+ hindex).addClass('hole'+ hindex);
+//            hindex++;
+//        }
+//        for (var i = 9; i < 19; i++) {
+//            $('.editablegrid-hole'+ hindex).addClass('hide hole' + hindex);
+//            hindex++;
+//        }
+//    }
+//    rindex++;
+//    hindex = 1;
+//}
 function settings() {
     var id = '#dialog';
 
@@ -191,7 +209,20 @@ function settings() {
 }
 $(document).ready(settings());
 // need a function to addClass to
-
+function removeTees(tee) {
+    if (tee == "women") {
+        $('.women').removeClass('hide').addClass('show-cell');
+    }
+    if (tee == "men") {
+        $('.men').removeClass('hide').addClass('show-cell');
+    }
+    if (tee == "champion") {
+        $('.champion').removeClass('hide').addClass('show-cell');
+    }
+    if (tee == "professional") {
+        $('.professional').removeClass('hide').addClass('show-cell');
+    }
+}
 function addClass() {
     $('.totals9').addClass('hide');
     $('.hole10').removeClass('hide').addClass('show-cell');
@@ -221,7 +252,47 @@ function removeClass(holecount, tee) {
     addHoles(holecount);
     addTeeBox(holecount, tee);
 }
-//function Total (playerName) {
-//    document.getElementById("mike")
-//
-//}
+var pArray = [];
+function totalIt(id) {
+    var reindex = /^[^\d]*(\d+)/.exec(id);
+    var ri = reindex[1];
+    console.log(ri);
+    var pNum = Number($('#'+id).text());
+    pArray.push(pNum);
+
+}
+function updateTotals() {
+    var rowLength = $("#R1" ).children().length;
+    console.log(rowLength);
+}
+var pindex ="5";
+function addRow () {
+    var holed = document.getElementById('holecount').value;
+    var newplayer = "<tr id=player" + pindex + "></tr>";
+    $('#playersTable').append(newplayer);
+    if (holed == 9 || holed == undefined || holed == '') {
+        for (var i = 1; i <= 10; i++) {
+            var new9td = "<td id=p" + pindex + "h" + i +" class=hole" + i + " contenteditable='true' onblur='totalIt(this.id)'></td>";
+            $('#player'+ pindex).append(new9td);
+        }
+        $('#player'+ pindex).append("<td id=p" + pindex + "totals9 class='show-cell totals9' contenteditable='false'></td>");
+        for (var i = 10; i <= 18; i++) {
+            var new9td = "<td id=p" + pindex + "h" + i +" class=hide hole" + i + " contenteditable='true' onblur='totalIt(this.id)'></td>";
+            $('#player'+ pindex).append(new9td);
+        }
+        $('#player'+ pindex).append("<td id=p" + pindex + "totals18 class='totals18 hide' contenteditable='false'></td>");
+    }
+    if (holed == 18) {
+        for (var i = 1; i <= 10; i++) {
+            var new9td = "<td id=p" + pindex + "h" + i +" class=hole" + i + " contenteditable='true' onblur='totalIt(this.id)'></td>";
+            $('#player'+ pindex).append(new9td);
+        }
+        $('#player'+ pindex).append("<td id=p" + pindex + "totals9 class='hide totals9' contenteditable='false'></td>");
+        for (var i = 10; i <= 18; i++) {
+            var new9td = "<td id=p" + pindex + "h" + i +" class=show-cell hole" + i + " contenteditable='true' onblur='totalIt(this.id)'></td>";
+            $('#player'+ pindex).append(new9td);
+        }
+        $('#player'+ pindex).append("<td id=p" + pindex + "totals18 class='totals18 show-cell' contenteditable='false'></td>");
+    }
+    pindex++;
+}
